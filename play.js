75d09d84ba22c1800835b8f98fe6650e0098a8d7
TedP <ted.pampilon@gmail.com>
2021-03-10 19:51:44 +0000

    made a separate client.js module and connected it to play.js, the code remains the same.

--- play.js
+++ play.js
@@ -1,27 +1,5 @@
-const net = require('net');
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
-  conn.on('close', () => { 
-    console.log('Client closed'); 
-  });  
-  
-  conn.setTimeout(3000);
-  conn.on('timeout', () => {
-    console.log('you ded cuz you idled');
-    conn.end();
-  });
-
-  return conn;
-}
+const connect = require('./client');
 
 console.log('Connecting ...');
+
 connect();
