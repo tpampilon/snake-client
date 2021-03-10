536b826238a64e183bbaac7b4cdb66ed8d82a520
TedP <ted.pampilon@gmail.com>
2021-03-10 20:28:08 +0000

    added initials and connection message in client.js

--- client.js
+++ client.js
@@ -1,18 +1,23 @@
 const net = require('net');
+const clientName = 'UMR';
 
 const connect = function() {
   const conn = net.createConnection({ 
     host: '10.0.2.15',
     port: 50541
   });
-  // interpret incoming data as text
   conn.setEncoding('utf8'); 
   
+  conn.on('connect', () => {
+    conn.write(`Name: ${clientName}`);
+    console.log(`Successfully connected to game server`);
+  });
+
   conn.on('close', () => { 
     console.log('Client closed'); 
   });  
   
-  conn.setTimeout(3000);
+  conn.setTimeout(5000);
   conn.on('timeout', () => {
     console.log('you ded cuz you idled');
     conn.end();
